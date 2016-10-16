01f0df6f93dcad3f9d0bf491135a3e368376e2f2
Aaron Abbott <aabmass@gmail.com>
2016-10-16 03:22:55 -0400

    Added in rest framework JWT

--- print_web_django/api/urls.py
+++ print_web_django/api/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
         namespace='rest_framework')),
+
+    url(r'^auth/login$', obtain_jwt_token),
+    url(r'^auth/refresh$', refresh_jwt_token),
 ]
