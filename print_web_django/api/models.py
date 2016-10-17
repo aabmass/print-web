46eb155833a6a973735fe3a4c5284836f3eac8c0
Aaron Abbott <aabmass@gmail.com>
2016-10-17 12:55:30 -0400

    Created a print job model

--- print_web_django/api/models.py
+++ print_web_django/api/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class PrintJob(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    last_printed = models.DateField()
+
+    # file will go here
--- print_web_django/print_web_django/settings.py
+++ print_web_django/print_web_django/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+
     'rest_framework',
 
     'django.contrib.admin',
