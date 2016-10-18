668f52281046e0c32f986ea54af30477a16de169
Aaron Abbott <aabmass@gmail.com>
2016-10-18 16:11:32 -0400

    Some production settings
    Note that it defers to envars!

--- print_web_django/print_web_django/settings/production.py
+++ print_web_django/print_web_django/settings/production.py
@@ -1,1 +1,17 @@
+import os
+
 from .base import *
+
+DEBUG = False
+SECRET_KEY = os.environ['SECRET_KEY']
+STATIC_ROOT = os.environ['STATIC_ROOT']
+
+ALLOWED_HOSTS = [
+    'aabmass.it.cx'
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# this can probably be tuned better
+CONN_MAX_AGE = 60
