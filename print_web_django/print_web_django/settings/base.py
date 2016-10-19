390e1c30d3c64b1342345ff24eeb2a0967fd9bd6
Aaron Abbott <aabmass@gmail.com>
2016-10-19 17:45:03 -0400

    Longed JWT Expiration delta

--- print_web_django/print_web_django/settings/base.py
+++ print_web_django/print_web_django/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
@@ -142,4 +143,8 @@ REST_FRAMEWORK = {
     ],
 }
 
-JWT_AUTH = {}
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
+}
+
+
