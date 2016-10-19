5885215cf9d05d1efd4b7d51d8bd80e0898dd9b7
Aaron Abbott <aabmass@gmail.com>
2016-10-19 16:17:08 -0400

    MEDIA_ROOT and MEDIA_URL properly configured

--- print_web_django/print_web_django/settings/base.py
+++ print_web_django/print_web_django/settings/base.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# User uploads (media)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 ############### OTHER OPTIONS ###############
 
