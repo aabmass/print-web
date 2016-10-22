560dfcdc07560ab78a4f91b82ec2570ba8f62650
Aaron Abbott <aabmass@gmail.com>
2016-10-21 23:09:45 -0400

    Add react build to Django's discovered static files

--- print_web_django/print_web_django/settings/base.py
+++ print_web_django/print_web_django/settings/base.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# copy the react app from it's build dir
+STATICFILES_DIRS = os.path.join(BASE_DIR, '..', 'react-frontend', 'build')
+
 # User uploads (media)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
