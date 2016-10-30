89163aed2bd06408626db48bc3edfb26263667f2
Aaron Abbott <aabmass@gmail.com>
2016-10-29 22:37:00 -0400

    Moved admin to 'administrator'

--- print_web_django/print_web_django/urls.py
+++ print_web_django/print_web_django/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^administrator/', admin.site.urls),
     url(r'^api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
